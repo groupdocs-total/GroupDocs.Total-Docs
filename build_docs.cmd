c3fd8eb1f4434ad9cb538601f220f29d618d4f9f
Vladimir Litvinchik <vladimir.litvinchik@aspose.com>
2025-10-31 12:12:27 +0200

    Add python-net to build script

--- build_docs.cmd
+++ build_docs.cmd
@@ -4,6 +4,7 @@ call git submodule update --init --recursive
 call git submodule foreach git pull origin master
 xcopy java common\content\%PRODUCT%\java /s /e /Y /i
 xcopy net common\content\%PRODUCT%\net /s /e /Y /i
+xcopy python-net common\content\%PRODUCT%\python-net /s /e /Y /i
 xcopy _index.md common\content\%PRODUCT% 
 cd common
 call hugo server --config config-geekdoc.toml, show-feedback-config.toml
