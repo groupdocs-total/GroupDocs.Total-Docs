06252e86f102d500a3b3ebd6afa6e84f8aa35647
Vladimir Litvinchik <vladimir.litvinchik@aspose.com>
2026-03-31 15:23:53 +0300

    Update common to the latest version

--- build_docs.sh
+++ build_docs.sh
@@ -4,6 +4,7 @@ git submodule update --init --recursive
 git submodule foreach git pull origin master
 mkdir -p common/content/$PRODUCT/java && cp -r java common/content/$PRODUCT/
 mkdir -p common/content/$PRODUCT/net && cp -r net common/content/$PRODUCT/
+mkdir -p common/content/$PRODUCT/python-net && cp -r python-net common/content/$PRODUCT/
 cp -r _index.md common/content/$PRODUCT
 cd common
-hugo server, show-feedback-config.toml
+hugo server
